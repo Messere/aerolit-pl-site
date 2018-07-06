2c926ba3576d5b9be8a44f9a291fee6b539d62ed
Dariusz Sieradzki <Messere@users.noreply.github.com>
2018-07-06 08:28:09 +0200

    cleanup server.js

--- server.js
+++ server.js
@@ -6,10 +6,7 @@ const http = require('http');
 const port = process.env.PORT || 7203;
 
 const app = express();
-
 app.use(express.static('./public/'));
-app.use(express.static('./src/'));
 
 const server = http.createServer(app);
-
 server.listen(port);
