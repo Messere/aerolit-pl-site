de4c3176c653783f9468c494b8744175c5634d42
Dariusz Sieradzki <Messere@users.noreply.github.com>
2018-07-06 12:22:47 +0200

    adjustments in default filesystem contents

--- src/files.ts
+++ src/files.ts
@@ -9,6 +9,7 @@ const openUrl = (url) => {
 const files = {
     'www': {
         'aerolit.pl': openUrl.bind(this, 'https://aerolit.pl/'),
+        'messer': openUrl.bind(this, 'http://messer.aerolit.pl/'),
         'montypython': openUrl.bind(this, 'https://montypython.aerolit.pl/'),
         'niemen': openUrl.bind(this, 'https://niemen.aerolit.pl/forum/'),
         'osjan': openUrl.bind(this, 'https://osjan.aerolit.pl/'),
@@ -16,13 +17,17 @@ const files = {
     'sites': {
         'hosted' : {
             'pikantnasztuka': openUrl.bind(this, 'https://pikantnasztuka.aerolit.pl/'),
-            'dziwnonoc': openUrl.bind(this, 'https://dziwnonoc.aerolit.pl/')
+            'dziwnonoc': openUrl.bind(this, 'https://dziwnonoc.aerolit.pl/'),
         },
         'external' : {
-
+            'facebook': openUrl.bind(this, 'https://www.facebook.com/darek.sieradzki.1'),
+            'linkedin': openUrl.bind(this, 'https://www.linkedin.com/in/dsier/'),
+            'github': openUrl.bind(this, 'https://github.com/Messere'),
+            'twitter': openUrl.bind(this, 'https://twitter.com/DarekSieradzki'),
         }
     },
-    'readme.txt': 'Hello world!',
+    'readme.txt': "Written as an excersise in TypeScript programming.\n\nReleased under MIT.\nTo see/fork source code execute /src",
+    'src': openUrl.bind(this, 'https://github.com/Messere'),
 };
 
 export default files;
